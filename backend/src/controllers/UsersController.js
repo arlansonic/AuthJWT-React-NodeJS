242e5d596f6147f65b6a1fbc8a513d56d8b67934
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-08-24 20:00:31 -0400

    Criando rota de Update e Delete dos usuários

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -52,10 +52,41 @@ class UserController {
 
     async update(req, res) {
 
+        try {
+            const { id } = req.params
+            const { name, email, password } = req.body
+
+            const user = await User.findById(id)
+
+            if (!user) {
+                return res.status(404).json({ message: "User not Found!" })
+            }
+            const passwordHash = createPasswordHash(password)
+            await user.update({ name, email, password: passwordHash })
+
+            return res.status(200).json({ message: 'User successfully updated ' })
+
+        } catch (error) {
+            console.error(err)
+            return res.status(500).json({ error: "Internal Server Error!!" })
+        }
     }
 
     async delete(req, res) {
+        try {
+            const { id } = req.params
+            const user = await User.findById(id)
 
+            if (!user) {
+                return res.status(404).json({ message: "User not Found!" })
+            }
+            await user.deleteOne()
+
+            return res.status(200).json({ message: "User successfully deleted" })
+        } catch (error) {
+            console.error(err)
+            return res.status(500).json({ error: "Internal Server Error!!" })
+        }
     }
 
 }
