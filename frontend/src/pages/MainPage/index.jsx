27f857c23911ce63ba7bda6a58c21717225645cd
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-08-31 23:08:37 -0400

    Criando funcionalidade para o Front, API e usabilidades de todas as ações da tela

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -1,24 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Nav from "./Nav";
 import Search from "./Search";
 import Repositories from "./Repositories";
+import { getRepositories, createRepository, DeleteRepository } from '../../services/api'
 import './style.css'
+const userId = '6308489647e35bf43c400be3'
 
 const MainPage = () => {
+    const [repositories, setRepositories] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [loadingError, setLoadingError] = useState(false)
+
+    const loadData = async (query = '') => {
+        try {
+            setLoading(true)
+            const res = await getRepositories(userId, query)
+            setRepositories(res.data)
+            setLoading(false)
+        } catch (err) {
+            console.error(err)
+            setLoadingError(true)
+        }
+    }
+
+    useEffect(() => {
+        (async () => await loadData())()
+    }, [])
+
     const handleLogout = () => {
         console.log('Logout')
     }
 
-    const handleSearch = (query) => {
+    const handleSearch = async (query) => {
         console.log('Query', query)
+        await loadData(query)
     }
 
-    const handleDeleteRepo = (repository) => {
+    const handleDeleteRepo = async (repository) => {
         console.log('Deletar Repo', repository)
+        await DeleteRepository(userId, repository._id)
+        await loadData()
     }
 
-    const handleNewRepo = (url) => {
+    const handleNewRepo = async (url) => {
         console.log('New Repo', url)
+        try {
+            await createRepository(userId, url)
+            await loadData()
+        } catch (err) {
+            console.error(err)
+            setLoadingError(true)
+        }
+    }
+
+    if (loadingError) {
+        return (
+            <div className="loading">
+                Erro ao carregar os dados do Repositório <Link to="/login">Voltar</Link>
+            </div>
+        )
+    }
+
+    if (loading) {
+        return (
+            <div className="loading">
+                <h1>Carregando........</h1>
+            </div>
+        )
     }
 
     return (
@@ -28,7 +77,7 @@ const MainPage = () => {
             <Search onSearch={handleSearch} />
             {/* Repositórios */}
             <Repositories
-                repositories={[]}
+                repositories={repositories}
                 onDeleteRepo={handleDeleteRepo}
                 onNewRepo={handleNewRepo}
             />
---
+++ frontend/src/services/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: 'http://10.57.72.121:3010',
+})
+
+export const getRepositories = async (userId, query) => {    
+    let url = `/users/${userId}/repositories/`
+
+    if (query !== '') {
+        url += `?q=${query}`
+    }
+
+    console.log('Query: ', url)
+
+    return api.get(url)
+}
+
+export const createRepository = async (userId, repositoryUrl) => {
+    const repositoryName = getRepositoryName(repositoryUrl)
+    const url = `/users/${userId}/repositories/`
+
+    return api.post(url, { name: repositoryName, url: repositoryUrl })
+}
+
+export const DeleteRepository = async (userId, id) => {
+    const url = `/users/${userId}/repositories/${id}`
+    return api.delete(url)
+}
+
+const getRepositoryName = (url) => {
+    const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
+
+    const match = url.match(regex)    
+
+    if(match[2]){
+        const values = match[2].split('/')        
+        return `${values[1]}/${values[2]}`
+    }    
+}
+
